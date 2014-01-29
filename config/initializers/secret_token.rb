f1e064e9bb51abaaa50bc4fc32ed254b118c2d8b
Megan Ferland <meganferland@gmail.com>
2014-01-29 10:37:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pegasus::Application.config.secret_key_base = '8458504f8ed2819903dfdbc426e8de54451625a18633ba415ae503bcb583916e2d64cbe19476e17ca1ca981f1611962316b6f8c5b5063f82f43542cab3dc05a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pegasus::Application.config.session_store :cookie_store, key: '_pegasus_session'
